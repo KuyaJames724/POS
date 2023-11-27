6a80f58c4063d3579e6044fb119c4387c384c2e5
Ryan Villaluz <97486781+villaluzr20@users.noreply.github.com>
2023-11-27 14:09:36 -0500

    Updated main file codes from Java to C#

---
+++ POS.cpp.fs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public string? name { get; set; }
+    public double price { get; set; }
+    public int Quantity { get; set; }
+    public int ID { get; set; }
+    public int QuantitySold { get; set; }
+
+    public void IncreaseQuantity(int amount)
+    {
+        this.Quantity += amount;
+    }
+
+    public void DecreaseQuantity (int amount)
+    {
+        if (this.Quantity >= amount)
+        {
+            this.Quantity -= amount;
+            this.QuantitySold += amount;
+
+        }
+        else
+        {
+            Console.WriteLine("Not enough in stock to sell this amount.");
+        }
+
+        // Calculate total sales
+        double CalculateTotalSales()
+        {
+            return this.price * this.QuantitySold;
+        }
+    }
+
+    public class Layout
+    {
+        public string? name { get; set; }
+        public string[]? Products { get; set; }
+        public string[]? ID { get; set; }
+    }
+
+    class Program
+    {
+        static Dictionary<string, double?> products = new Dictionary<string, double?>()
+        {
+            {"Arroz Con Pollo", 16.00},
+            {"Bulgogi Tacos", 14.00},
+            {"Ceviche", 12.00},
+            {"Chimichanga", 14.00},
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the POS System!");
+            Console.WriteLine("Please enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+
+            Console.WriteLine("Please enter your password: ");
+            string? password = Console.ReadLine();
+            if (password == "password")
+            {
+                Console.WriteLine("Welcome to the POS System!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password. Please try again.");
+            }
+
+            Console.WriteLine("Please enter the name of the product you would like to purchase: ");
+            string? product = Console.ReadLine();
+            Console.WriteLine("Please enter the quantity of the product you would like to purchase: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            if (products.ContainsKey(product))
+            {
+                Console.WriteLine("Your total is: " + products[product] * quantity);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, we do not have that product.");
+            }
+
+            Console.WriteLine("Thank you for shopping with us! Please come again soon!");
+        }
+
+    }
+}
